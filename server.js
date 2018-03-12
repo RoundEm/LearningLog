63ef373b884fea0643564f8dbf151e784fd1a836
Jason Roundtree <jasontree123@gmail.com>
2018-03-11 19:57:10 -0700

    Add get route for manage logs page

--- server.js
+++ server.js
@@ -10,10 +10,14 @@ app.use(express.static('public'));
 const { JournalEntries } = require('./models');
 
 app.get('/', (req, res) => {
-	res.sendFile(_dirname, 'public/index.html');
+	res.sendFile(`${__dirname}/views/index.html`);
 });
 
-// get all of your posts once you login
+app.get('/journalEntries', (req, res) => {
+	res.sendFile(`${__dirname}/views/logs.html`);
+});
+
+// get all of your posts
 app.get('/journalEntries', (req, res) => {
 	res.json(JournalEntries.get());
 });
@@ -24,23 +28,30 @@ app.get('/journalEntry/:entry_id', (req, res) => {
 });
 
 app.post('/journalEntries', jsonParser, (req, res) => {
-
+	const entry = JournalEntries.create(req.body.title, req.body.content);
+	res.status(201).json(entry);
 });
 
 app.put('/journalEntries/:entry_id', jsonParser, (req, res) => {
-
+	const revisedEntry = JournalEntries.update({
+		id: req.params.entry_id,
+		title: req.body.title,
+		content: req.body.content,
+		publishDate: req.body.publishDate
+	});
+	res.status(204).end();
 });
 
 app.delete('/journalEntries/:entry_id', (req, res) => {
-
+	JournalEntries.delete(req.params.id);
+	console.log(`Deleted journal entry \`${req.params.entry_id}\``)
+	res.status(204).end();
 });
 
 
 JournalEntries.create('Title A', 'Lorem ipsum dolor sit amet');
 JournalEntries.create('Title B', 'Duis aute irure dolor in reprehenderit ');
 JournalEntries.create('Title C', 'Excepteur sint occaecat cupidatat non proident');
-
-
 
 
 let server;
