d1a7ee1d8c28f9d028a4adf194586ff3efa111f4
Jason Roundtree <jasontree123@gmail.com>
2018-03-13 13:52:25 -0700

    Display data in browser

--- public/ajax.js
+++ public/ajax.js
@@ -4,18 +4,36 @@ const Ajax = {
 	getData: function() {
 		console.log('getData ran');
 		return $.getJSON( "/logEntries", function( data ) {
-			console.log('getData data:', data);
 			return data;
 		});
 	},
-	renderLogPage: function() {
+	processLogData: function() {
+		console.log('processLogData ran');
 		let data = Ajax.getData().then(function(data) {
-			console.log('Render log page data', data);
-		});
+			Ajax.displayData(data);
+		});   
+	},
+	displayData: function(data) {
+		console.log('displayData:', data);
+		for (let i = 0; i < data.length; i++) {
+			let logContent = data[i].content;
+			let logDate = data[i].publishDate;
+			let logTitle = data[i].title;
+			let logTag = data[i].tag;
+			$('.logList-section').append(`
+				<div>
+					<p>${logTitle}</p>
+					<p>${logDate}</p>
+					<p>${logContent}</p>
+					<p>${logTag}</p>
+				</div>
+				`);
+		}
+		
 	},
 	setup: function() {
-		Ajax.renderLogPage();
 		console.log('setup ran');
+		Ajax.processLogData();
 	}
 }
 
