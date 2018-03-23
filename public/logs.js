c236951a92e24db97c4d29a5825429a58351a9a4
Jason Roundtree <jasontree123@gmail.com>
2018-03-23 14:44:28 -0700

    Fix tag sorting to change tags to lowercase prior to sorting

--- public/logs.js
+++ public/logs.js
@@ -35,19 +35,21 @@ const Logs = {
 		});   
 	},
 	displayData: function(logData) {
-		console.log('displayData:', logData);
+		// console.log('displayData:', logData);
 		let logListSections = '';
 		for (let i = 0; i < logData.length; i++) {
 			let logContent = logData[i].content;
 			let logDateTime = logData[i].dateTime;
 			let logTitle = logData[i].title;
 			let logTag = logData[i].tag;
+			let logID = logData[i].id;
 			logListSections +=
 				`<div class="logEntry">	
 					<p>${logTitle}</p>
 					<p>${logTag}</p>
 					<p>${logContent}</p>
 					<p>${logDateTime}</p>
+					<p class="entryId" hidden>${logID}</p>
 				</div>`;
 		}
 		$('.logList-section').empty().append(logListSections);
@@ -55,8 +57,7 @@ const Logs = {
 	sortLogs: function() {
 		$('select').change(function() {
 			let sortValue = $(this).val();
-			// console.log('sortValue:', sortValue);
-			// console.log('Logs.logData:', Logs.logData);
+			console.log('sortValue:', sortValue);
 			if (sortValue === 'log_newest') {
 				Logs.logData.sort(function(a, b) {
 					return b.publishDate - a.publishDate;
@@ -67,23 +68,49 @@ const Logs = {
 				});
 			} else if (sortValue === 'tag_a') {
 				Logs.logData.sort(function(a, b) {
-					if (a.tag < b.tag) return -1;
-					if (a.tag > b.tag) return 1;
+					let _a = a.tag.toLowerCase();
+					let _b = b.tag.toLowerCase();
+					console.log(_a);
+					if (_a < _b) return -1;
+					if (_a > _b) return 1;
 					return 0;
 				});
 			} else {
 				Logs.logData.sort(function(a, b) {
-					if (b.tag < a.tag) return -1;
-					if (b.tag > a.tag) return 1;
+					let _a = a.tag.toLowerCase();
+					let _b = b.tag.toLowerCase();
+					console.log(_a);
+					if (_b < _a) return -1;
+					if (_b > _a) return 1;
 					return 0;
 				});
 			}
 			Logs.displayData(Logs.logData);
 		});
 	},
+	viewLog: function(entry_id) {
+		return $.getJSON(`/logEntry/${entry_id}`, function(data) {
+			console.log(data);
+		});
+	},
+	// deleteLog: function() {
+
+	// },
+	// updateLog: function() {
+
+	// },
+	bindLogClick: function() {
+		$('.logList-section').on('click', '.logEntry', function() {
+			let entryId = $(this).find('.entryId').text();
+			console.log('entryId:', entryId);
+			Logs.viewLog(entryId);
+		});
+	},
 	setup: function() {
 		Logs.processLogData();
 		Logs.sortLogs();
+		Logs.bindLogClick();
+		Logs.viewLog();
 	}
 }
 
