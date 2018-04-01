27fa3f48aa9cf98778666e963888a2429a749f85
Jason Roundtree <jasontree123@gmail.com>
2018-04-01 15:57:24 -0700

    Add all ajax calls to this page instead of logs.js

--- public/Data.js
+++ public/Data.js
@@ -1,11 +1,63 @@
 var Data = {
-	getLogs: function () {
-
-	},
 	getLog: function (logId, cb) {
-		$.getJSON(`/logEntry/${logId}`, cb);
+		$.ajax({
+			url: `/logEntries/${logId}`,
+			type: 'GET',
+			success: function(res) {
+		   		cb(null, res);
+		   	},
+		   	error: function(err) {
+		   		cb(err);
+		   	}
+		})
+	},
+	getLogs: function (cb) {
+		$.ajax({
+			url: '/logEntries',
+			type: 'GET',
+			success: function(res) {
+				cb(null, res);
+			},
+			error: function() {
+				cb(err);
+			}
+		});
+	},
+	postLog: function(cb) {
+		$.ajax({
+			url: '/logEntries',
+			type: 'POST',
+			success: function(res) {
+		   		cb(null, res);
+		   	},
+		   	error: function(err) {
+		   		cb(err);
+		   	}
+		})
 	},
 	updateLog: function(logId, newLog, cb) {
-		
+		$.ajax({
+		   url: `/logEntries/${logId}`,
+		   type: 'PUT',
+		   data: newLog,
+		   success: function(res) {
+		   		cb(null, res);
+		   },
+		   error: function(err) {
+		   		cb(err);
+		   }
+		});
+	},
+	removeLog: function(logId, cb) {
+		$.ajax({
+		   url: `/logEntries/${logId}`,
+		   type: 'DELETE',
+		   success: function(res) {
+		   		cb(null, res);
+		   },
+		   error: function(err) {
+		   		cb(err);
+		   }
+		});
 	}
 }
