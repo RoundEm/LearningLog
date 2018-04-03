5538fa76cc254f212f081797dece565f62f85165
Jason Roundtree <jasontree123@gmail.com>
2018-04-03 12:45:22 -0700

    Refactor code to no longer reference logs.js file

--- public/viewLog.js
+++ public/viewLog.js
@@ -1,12 +1,42 @@
-// Logs.viewLogData()
-	// .then(Logs.editLog());
+'use strict'
 	
 const viewLogId = window.location.pathname.split('/')[2];
 
-function initViewLog() {
-	Data.getLog(viewLogId, function(err, data) {
-		Logs.displayLogsData([data]);
-	});	
+function displayLogData(err, data) {
+	let dataArray = [data];
+	if (err) {
+		return console.log(err);
+	} 
+	console.log('displayLogData data:', dataArray);
+	let logList = '';
+	for (let i = 0; i < dataArray.length; i++) {
+		let logContent = dataArray[i].content;
+		let logDateTime = dataArray[i].dateTime;
+		let logTitle = dataArray[i].title;
+		let logTag = dataArray[i].tag;
+		let logID = dataArray[i].id;
+		logList +=
+			`<div class="logEntry">	
+				<p>${logTitle}</p>
+				<p>${logTag}</p>
+				<p>${logContent}</p>
+				<p>${logDateTime}</p>
+				<p class="entryId" hidden>${logID}</p>
+			</div>`;
+	}
+	$('.render-log-section').empty().append(logList);
 }
 
-initViewLog();
+function bindEditBtn() {
+	$('#editLog').click(function() {
+		let entryId = $(this).siblings('.render-log-section').find('.entryId').text();
+		window.location.href = `/edit-log/${entryId}`;
+	});
+}
+
+function initViewLog() {
+	bindEditBtn();
+	Data.getLog(viewLogId, displayLogData);	
+}
+
+$(initViewLog);
