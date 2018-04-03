da79d618aacc78c59192ea669e6fd7178f43da87
Jason Roundtree <jasontree123@gmail.com>
2018-04-03 12:46:30 -0700

    Refactor code to no longer reference logs.js file

--- public/viewLogs.js
+++ public/viewLogs.js
@@ -1,9 +1,10 @@
-// $(Logs.setupViewLogs);
+'use strict'
+
 let logsData = {}
 
 function processLogData(data) {
 		logsData = data;
-		console.log('data:', data);
+		console.log('processLogData data:', data);
 		const daysOfWeek = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
 		for (let i = 0; i < data.length; i++) {
 			let d = new Date(data[i].publishDate);
@@ -21,15 +22,15 @@ function processLogData(data) {
 			let dayOfWeek = daysOfWeek[dayOfWeekIndex];
 			logsData[i].dateTime = `${dayOfWeek} - ${month}/${dateOfMonth}/${year} - ${hour}:${minutes}:${milliSecs}`;
 		}
-		displayLogData(logsData);
+		displayLogsData(logsData);
 }
 
-function displayLogData(data) {
+function displayLogsData(data) {
 	console.log('displayLogsData data:', data);
 	let logList = '';
 	for (let i = 0; i < data.length; i++) {
 		let logContent = data[i].content;
-		let logDateTime = data[i].publishDate;
+		let logDateTime = data[i].dateTime;
 		let logTitle = data[i].title;
 		let logTag = data[i].tag;
 		let logID = data[i].id;
@@ -45,14 +46,11 @@ function displayLogData(data) {
 	$('.render-log-section').empty().append(logList);
 }
 
-
 function bindHandlers() {
 	$('.render-log-section').on('click', '.logEntry', function() {
 		let entryId = $(this).find('.entryId').text();
 		console.log('entryId:', entryId);
-
-		// pass object to getLogData request 
-		window.location.href = '/view-log';
+		window.location.href = `/view-log/${entryId}`;
 	});
 
 	// sorting logs
@@ -60,15 +58,15 @@ function bindHandlers() {
 		let sortValue = $(this).val();
 		console.log('sortValue:', sortValue);
 		if (sortValue === 'log_newest') {
-			Logs.logsData.sort((a, b) => {
+			logsData.sort((a, b) => {
 				return b.publishDate - a.publishDate;
 			});
 		} else if (sortValue === 'log_oldest') {
-			Logs.logsData.sort((a, b) => {
+			logsData.sort((a, b) => {
 				return a.publishDate - b.publishDate;
 			});
 		} else if (sortValue === 'tag_a') {
-			Logs.logsData.sort((a, b) => {
+			logsData.sort((a, b) => {
 				let _a = a.tag.toLowerCase();
 				let _b = b.tag.toLowerCase();
 				if (_a < _b) return -1;
@@ -76,7 +74,7 @@ function bindHandlers() {
 				return 0;
 			});
 		} else {
-			Logs.logsData.sort((a, b) => {
+			logsData.sort((a, b) => {
 				let _a = a.tag.toLowerCase();
 				let _b = b.tag.toLowerCase();
 				if (_b < _a) return -1;
@@ -84,25 +82,13 @@ function bindHandlers() {
 				return 0;
 			});
 		}
-		// pass sorted object somewhere so it can be rendered
-	});
-	$('#editLog').click(function() {
-		let entryId = $(this).siblings('.render-log-section').find('.entryId').text();
-		window.location.href = `/edit-log/${entryId}`;
+		displayLogsData(logsData);
 	});
 }
 
 function initViewLogs() {
 	bindHandlers();
 	Data.getLogs(processLogData);
-
 }
-
-// function initViewLogs() {
-// 	bindHandlers();
-// 	Data.getLogs(function(err, data) {
-// 		Logs.displayLogData(data);
-// 	});
-// }
 
 $(initViewLogs);
