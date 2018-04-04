0a3dc34c66c75bea6ba538dd90258b60b15e1ea0
Jason Roundtree <jasontree123@gmail.com>
2018-04-04 14:32:29 -0700

    Fix renamed target classes and refactor for other updates and non-namespaced code

--- public/addLogs.js
+++ public/addLogs.js
@@ -1,34 +1,33 @@
 'use strict';
 
-const AddLogs = {
-	createLogEntry: function(title, content, tag) {
-		console.log('createLogEntry ran');
-		const params = {
-			title: title,
-			content: content,
-			tag: tag
-		}
-		// return $.post('/logEntries', params);
-		Data.postLog(params);
-	},
-	bindSubmitBtn: function() {
-		$('.add-log').submit(function(event) {
-			event.preventDefault();
-			let titleTarget = $(this).find('#add-title');
-			let titleValue = titleTarget.val();
-			let contentTarget = $(this).find('#add-content');
-			let contentValue = contentTarget.val();
-			let tagTarget = $(this).find('#add-tag');
-			let tagValue = tagTarget.val();
-			$('.add-log input, .add-log textarea').prop('readonly', true);
-			AddLogs.createLogEntry(titleValue, contentValue, tagValue).then(console.log('log successfully created'));	
-			$('#add-title, #add-content, #add-tag').css('background-color', '#fafafa');
-    		$('.successMsg').append(`Your log has been saved! You can <a href="/add-log">add another log</a> or <a href="/view-logs">return to your saved logs.`);
-		});	 
-	},
-	setup: function() {
-		AddLogs.bindSubmitBtn();
+function createLogEntry(title, content, tag) {
+	const params = {
+		title: title,
+		content: content,
+		tag: tag
 	}
+	Data.postLog(params);
 }
 
-$(AddLogs.setup);
+function bindSubmitBtn() {
+	$('.logForm').submit(function(event) {
+		event.preventDefault();
+		console.log('bindSubmitBtn ran')
+		let titleTarget = $(this).find('#add-title');
+		let titleValue = titleTarget.val();
+		let contentTarget = $(this).find('#add-content');
+		let contentValue = contentTarget.val();
+		let tagTarget = $(this).find('#add-tag');
+		let tagValue = tagTarget.val();
+		$('.logForm input, .logForm textarea').prop('readonly', true);
+		createLogEntry(titleValue, contentValue, tagValue);	
+		$('#add-title, #add-content, #add-tag').css('background-color', '#fafafa');
+		$('.successMsg').append(`Your log has been saved! You can <a href="/add-log">add another log</a> or <a href="/view-logs">return to your saved logs.`);
+	});
+}
+
+function initAddLogs() {
+	bindSubmitBtn();
+}
+
+$(initAddLogs);
