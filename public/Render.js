06d6fa6a81f804f051671f4f77282dad37918581
Jason Roundtree <jasontree123@gmail.com>
2018-04-04 16:56:33 -0700

    Separate date formatting into its own function and call it from renderLog and renderViewLog

--- public/Render.js
+++ public/Render.js
@@ -1,10 +1,43 @@
 'use strict'
 
 function renderLog(log) {
+	let dateTime = formatDateTime(log.publishDate);
+	return `<div class="logEntry">	
+				<p>${log.title}</p>
+				<p>${log.tag}</p>
+				<p>${log.content}</p>
+				<p>${dateTime}</p>
+				<p class="entryId" hidden>${log.id}</p>
+			</div>`;
+}
+
+function renderLogs(logs) {
+	return logs.map(function(log) {
+		return renderLog(log);
+	});
+}
+
+function renderViewLog(log) {
+	let dateTime = formatDateTime(log.publishDate);
+	return `<div class="viewLog">
+				<span>Title</span>	
+				<p>${log.title}</p>
+				<span>Content</span>
+				<p>${log.content}</p>
+				<span>Tag</span>
+				<p>${log.tag}</p>
+				<span>Last Updated</span>
+				<p>${dateTime}</p>
+				<p class="entryId" hidden>${log.id}</p>
+			</div>`;
+}
+
+function formatDateTime(publishDate) {
+	console.log('publishDate:', publishDate);
 	const daysOfWeek = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
 	// '+' in new Date constructor ensures that the date is converted from a 
 	// string to a integer when coming back from editLog page
-	let d = new Date(+log.publishDate);
+	let d = new Date(+publishDate);
 	let hour = d.getHours();
 	let minutes = d.getMinutes();
 	if (minutes < 10) {
@@ -18,31 +51,6 @@ function renderLog(log) {
 	let dayOfWeekIndex = d.getDay();
 	let dayOfWeek = daysOfWeek[dayOfWeekIndex];
 	const dateTime = `${dayOfWeek} - ${month}/${dateOfMonth}/${year} - ${hour}:${minutes}:${milliSecs}`;
-	return `<div class="logEntry">	
-				<p>${log.title}</p>
-				<p>${log.tag}</p>
-				<p>${log.content}</p>
-				<p>${dateTime}</p>
-				<p class="entryId" hidden>${log.id}</p>
-			</div>`;
+	return dateTime;
 }
 
-function renderViewLog(log) {
-	return `<div class="viewLog">
-				<span>Title</span>	
-				<p>${log.title}</p>
-				<span>Tag</span>
-				<p>${log.tag}</p>
-				<span>Content</span>
-				<p>${log.content}</p>
-				<span>Last Updated</span>
-				
-				<p class="entryId" hidden>${log.id}</p>
-			</div>`;
-}
-
-function renderLogs(logs) {
-	return logs.map(function(log) {
-		return renderLog(log);
-	});
-}
