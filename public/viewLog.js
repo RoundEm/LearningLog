486e005b009f36a1f49b2805941cf9074e7d4eeb
Jason Roundtree <jasontree123@gmail.com>
2018-04-03 17:39:48 -0700

    Add Return to All Logs button functionality

--- public/viewLog.js
+++ public/viewLog.js
@@ -3,7 +3,6 @@
 const viewLogId = window.location.pathname.split('/')[2];
 
 function handleGetLog(err, log) {
-	// console.log(err, log)
 	if (err) {
 		return console.log(err);
 	} 
@@ -14,6 +13,10 @@ function bindEditBtn() {
 	$('#editLog').click(function() {
 		let entryId = $(this).siblings('.render-log-section').find('.entryId').text();
 		window.location.href = `/edit-log/${entryId}`;
+	});
+
+	$('#returnBtn').click(function() {
+		window.location.href = '/view-logs';
 	});
 }
 
