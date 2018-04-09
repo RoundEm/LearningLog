3b0fa45f9900a9ed5a2a0918718ec437e8dae02c
Jason Roundtree <jasontree123@gmail.com>
2018-04-09 14:36:45 -0700

    Initial commit

--- config.js
+++ config.js
@@ -1,3 +1,13 @@
-exports.DB_URL = process.env.DATABASE_URL || 'mongodb://localhost/learning-log';
-exports.TEST_DB_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-learning-log';
+function getDbUrl() {
+	let url = '';
+	if (process.env.NODE_ENV === 'production') {
+		url = process.env.DATABASE_URL;
+	} else if (process.env.NODE_ENV === 'test') {
+		url = 'mongodb://localhost/test-learning-log';
+	} 
+	return url || 'mongodb://localhost/learning-log';
+}
+
 exports.PORT = process.env.PORT || 8080;
+
+exports.DB_URL = getDbUrl();
