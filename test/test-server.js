12476dc98c23b12e578739b0ee5743c7712c5077
Jason Roundtree <jasontree123@gmail.com>
2018-04-10 11:56:08 -0700

    POST route test completed and passing

--- test/test-server.js
+++ test/test-server.js
@@ -25,8 +25,8 @@ describe('My app', function() {
 				expect(res.body).to.be.a('array');
 				expect(res).to.be.json;
 				res.body.forEach(function(log) {
-					expect(log).to.be.a.object;
-					expect(item).to.have.all.keys('id', 'title', 'content', 'tag');
+					expect(log).to.be.a('object');
+					expect(item).to.include.keys('id', 'title', 'content', 'tag');
 				})
 			});
 	});
@@ -35,15 +35,27 @@ describe('My app', function() {
 	// 		.get('/logEntries/:logId')
 	// 		.then(function(res) {
 	// 			expect(res).to.have.status(200);
+	// 			expect(res.body).to.be.a('object');
 	// 		});
 	// });
-	// it('should create a new log on POST', function() {
-	// 	return chai.request(app)
-	// 		.post('/logEntries/:logId')
-	// 		.then(function(res) {
-	// 			expect(res).to.have.status(201);
-	// 		});
-	// });
+	it('should create a new log on POST', function() {
+		return chai.request(app)
+			let newTestLog = {
+				title: 'test title',
+				content: 'test content',
+				tag: 'test tag'
+			}
+			.post('/logEntries')
+			.send(newTestLog)
+			.then(function(res) {
+				expect(res).to.have.status(201);
+				expect(res).to.be.json;
+				expect(res.body).to.be.a('object');
+				// is this even working correctly?? Test still passes when i test for date
+				expect(res.body.publishDate).to.be.a('string');
+				expect(res.body).to.include.keys('id', 'title', 'content', 'tag', 'publishDate');
+			});
+	});
 	// it('should update a log on PUT', function() {
 	// 	return chai.request(app)
 	// 		.put('/logEntries/:logId')
