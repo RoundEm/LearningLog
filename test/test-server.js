07b9fc7b30492f524020ddb47017c7fcfe47d6a6
Jason Roundtree <jasontree123@gmail.com>
2018-04-11 16:22:19 -0700

    All endpoints are passing tests

--- test/test-server.js
+++ test/test-server.js
@@ -4,71 +4,140 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 
 const { runServer, closeServer, app } = require('../server');
+const { seedLogs } = require('../seeder');
+const { Log } = require('../models');
 
-describe('My app', function() {
-	before(function() {
+function tearDownDb() {
+	console.log('Removing database');
+	return mongoose.connection.dropDatabase();
+}
+
+describe('My app', () => {
+
+	// Q: Should i pass test Db to runServer like thinkful example?
+	before(() => {
 		return runServer();
 	});
 
-	after(function() {
+	beforeEach((done) => {
+		const addSeedDataToDb = seedLogs(Log);
+		addSeedDataToDb.then((logs) => {
+			done(); 
+		});
+	});
+
+	afterEach(() => {
+		return tearDownDb();
+	});
+
+	after(() => {
 		return closeServer();
 	});
 
-	// beforeEach - clear db
-	// afterEach
+	describe('GET endpoint', () => {
+		it('should return all logs', () => {
+			return chai.request(app)
+				.get('/logEntries')
+				.then(function(res) {
+					expect(res).to.have.status(200);
+					expect(res.body).to.be.a('array');
+					expect(res).to.be.json;
+					res.body.forEach((log) => {
+						expect(log).to.be.a('object');
+						expect(log).to.include.keys('id', 'title', 'content', 'tag', 'publishDate');
+					});
+				});
+		});	
+	});
+	describe('GET endpoint', () => {
+		it('should list the correct log on GET by ID', () => {
+			let id = '';
+			return Log
+				.findOne()
+					.then((log) => {
+						id = log._id;
+						return chai.request(app) 
+							.get(`/logEntries/${id}`)
+							.then((res) => {
+								expect(res).to.have.status(200);
+								expect(res.body).to.be.a('object');
 
-	it('should list all logs on GET', function() {
-		return chai.request(app)
-			.get('/logEntries')
-			.then(function(res) {
-				expect(res).to.have.status(200);
-				expect(res.body).to.be.a('array');
-				expect(res).to.be.json;
-				res.body.forEach(function(log) {
-					expect(log).to.be.a('object');
-					expect(item).to.include.keys('id', 'title', 'content', 'tag');
-				})
-			});
+								// what else should I test for?
+							});
+					});
+		});
 	});
-	// it('should list a specific log on GET by id', function() {
-	// 	return chai.request(app)
-	// 		.get('/logEntries/:logId')
-	// 		.then(function(res) {
-	// 			expect(res).to.have.status(200);
-	// 			expect(res.body).to.be.a('object');
-	// 		});
-	// });
-	it('should create a new log on POST', function() {
-		return chai.request(app)
-			let newTestLog = {
-				title: 'test title',
-				content: 'test content',
-				tag: 'test tag'
-			}
-			.post('/logEntries')
-			.send(newTestLog)
-			.then(function(res) {
-				expect(res).to.have.status(201);
-				expect(res).to.be.json;
-				expect(res.body).to.be.a('object');
-				// is this even working correctly?? Test still passes when i test for date
-				expect(res.body.publishDate).to.be.a('string');
-				expect(res.body).to.include.keys('id', 'title', 'content', 'tag', 'publishDate');
-			});
+	describe('POST endpoint', () => {
+		it('should create a new log with the required fields', () => {
+
+			// should i change this to use generateLogData() instead??
+			const newTestLog = {
+				title: 'POST test title',
+				content: 'POST test content',
+				tag: 'POST test tag'
+			};
+			return chai.request(app)
+				.post('/logEntries')
+				.send(newTestLog)
+				.then((res) => {
+					expect(res).to.have.status(201);
+					expect(res).to.be.json;
+					expect(res.body).to.be.a('object');
+
+					// expect(res.body.publishDate).to.be.a('date');
+					expect(res.body).to.include.keys('id', 'title', 'content', 'tag', 'publishDate');
+
+					// check if this is working correctly
+					expect(res.body.title).to.equal(newTestLog.title);
+					expect(res.body.content).to.equal(newTestLog.content);
+					expect(res.body.tag).to.equal(newTestLog.tag);
+				});
+		});
 	});
-	// it('should update a log on PUT', function() {
-	// 	return chai.request(app)
-	// 		.put('/logEntries/:logId')
-	// 		.then(function(res) {
-	// 			expect(res).to.have.status(204);
-	// 		});
-	// });
-	// it('should remove a log on DELETE', function() {
-	// 	return chai.request(app)
-	// 		.delete('/logEntries/:logId')
-	// 		.then(function(res) {
-	// 			expect(res).to.have.status(204);
-	// 		});
-	// });
+	describe('PUT endpoint', () => {
+		it('should update the correct log data', () => {
+		const logToUpdate = {
+				title: 'PUT test title',
+				content: 'PUT test content',
+				tag: 'PUT test tag'
+			};	
+			return Log
+				.findOne()
+					.then((log) => {
+						logToUpdate.id = log._id;
+						return chai.request(app)
+							.put(`/logEntries/${log._id}`)
+							.send(logToUpdate)
+					})
+					.then((res) => {
+						expect(res).to.have.status(204);
+						expect(res.body).to.be.a('object');
+						return Log.findById(logToUpdate.id);
+					})
+					.then((log) => {
+						expect(log.title).to.equal(logToUpdate.title);
+						expect(log.content).to.equal(logToUpdate.content);
+						expect(log.tag).to.equal(logToUpdate.tag);
+					});
+		});
+	});
+	describe('DELETE endpont', () => {
+		it('should remove a log by ID', () => {
+			let log;
+			return Log
+				findOne()
+					.then((_log) => {
+						return chai.request(app).delete(`/logEntries/${log.id}`);
+					})
+					.then((res) => {
+						expect(res).to.have.status(204);
+						return Log.findById(log.id);
+					})
+					.then((_log) => {
+						expect(_log).to.be.null;
+					});
+		});
+	});	
 });
+
 
