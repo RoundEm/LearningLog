2dc4ed88421124698f7fe9f8a4174def7fc2cca1
Jason Roundtree <jasontree123@gmail.com>
2018-04-11 12:03:40 -0700

    Sort should now be fixed but need to also fix publishDate to reflect when a log is saved for sort by date to work

--- public/viewLogs.js
+++ public/viewLogs.js
@@ -6,51 +6,61 @@ function handleLogs(err, logs) {
 	if (err) {
 		return console.log(err);
 	}
-	// Assign data to logsData so it can be temporarily stored for sorting
 	logsData = logs;
-	const logsHTML = renderLogs(logs);
+	sortLogs(logsData);
+}
+
+function sortLogs(logsData, sortValue) {
+	console.log('sortValue:', sortValue);
+	console.log('logsData:', logsData);
+	if (sortValue === undefined) {
+		console.log('if ran');
+		// console.log('logs:', logs);
+		logsData.sort(function(a, b) {
+			return b.publishDate - a.publishDate;
+		});
+	} else if (sortValue === 'log_newest') {
+		logsData.sort(function(a, b) {
+			return b.publishDate - a.publishDate;
+		});
+	} else if (sortValue === 'log_oldest') {
+		console.log('log_oldest ran')
+		logsData.sort(function(a, b) {
+			return a.publishDate - b.publishDate;
+		});
+	} else if (sortValue === 'tag_a') {
+		logsData.sort(function(a, b) {
+			let _a = a.tag.toLowerCase();
+			let _b = b.tag.toLowerCase();
+			if (_a < _b) return -1;
+			if (_a > _b) return 1;
+			return 0;
+		});
+	} else {
+		logsData.sort(function(a, b) {
+			let _a = a.tag.toLowerCase();
+			let _b = b.tag.toLowerCase();
+			if (_b < _a) return -1;
+			if (_b > _a) return 1;
+			return 0;
+		});
+	}
+	const logsHTML = renderLogs(logsData);
 	$('.render-log-section').empty().append(logsHTML.join(''));
+
 }
 
 function bindHandlers() {
-
 	// Grab entry ID for targeting during log click
 	$('.render-log-section').on('click', '.logEntry', function() {
 		let logId = $(this).find('.logId').text();
 		window.location.href = `/view-log/${logId}`;
 	});
 
-	// Sort logs
+	// Grab selected sort-by value 
 	$('select').change(function() {
 		let sortValue = $(this).val();
-		console.log('sortValue:', sortValue);
-		if (sortValue === 'log_newest') {
-			logsData.sort((a, b) => {
-				return b.publishDate - a.publishDate;
-			});
-		} else if (sortValue === 'log_oldest') {
-			logsData.sort((a, b) => {
-				return a.publishDate - b.publishDate;
-			});
-		} else if (sortValue === 'tag_a') {
-			logsData.sort((a, b) => {
-				let _a = a.tag.toLowerCase();
-				let _b = b.tag.toLowerCase();
-				if (_a < _b) return -1;
-				if (_a > _b) return 1;
-				return 0;
-			});
-		} else {
-			logsData.sort((a, b) => {
-				let _a = a.tag.toLowerCase();
-				let _b = b.tag.toLowerCase();
-				if (_b < _a) return -1;
-				if (_b > _a) return 1;
-				return 0;
-			});
-		}
-
-		handleLogs(null, logsData);
+		sortLogs(logsData, sortValue);
 	});
 }
 
