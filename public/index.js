5f2abbcd1e57038fda24d8194f973188562b7f5b
Jason Roundtree <jasontree123@gmail.com>
2018-04-17 15:14:19 -0700

    Add handlers for highlighting of link div when the link is tabbed to

--- public/index.js
+++ public/index.js
@@ -1,8 +1,14 @@
-function bindButton() {
-	$('.new-page').keypress(function(event) {
+	function bindButton() {
+	$('.new-page').keypress((event) => {
 		if (event.which === 13) {
 			window.location.href = '/view-logs'
 		}
+	});
+	$('.new-page a').focus(function(event) {
+		$('.new-page').css('background-color', '#ffde4d');
+	});
+	$('.new-page a').blur((event) => {
+		$('.new-page').css('background-color', 'white');
 	});
 }
 
