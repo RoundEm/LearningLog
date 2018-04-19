37538787abac916bb8b2f05a277b6ba1e7d06a4a
Jason Roundtree <jasontree123@gmail.com>
2018-04-19 12:26:40 -0700

    Move Types object from Types.js to this models

--- models.js
+++ models.js
@@ -28,13 +28,25 @@ const LogSchema = new Schema({
 	}
 });
 
+const Types = {
+	concept: 'Concepts',
+	question: 'Questions',
+	example: 'Examples',
+	goal: 'Goals & Objectives',
+	improvement: 'Needs Improvement',
+	resource: 'Resources',
+	analogy: 'Analogies & Connections',
+	accomplishment: 'Accomplishments & Inspiration',
+	idea: 'New Ideas'
+}
+
 LogSchema.methods.serialize = function() {
 	return {
 		title: this.title,
 		content: this.content,
 		publishDate: this.publishDate,
 		tag: this.tag,
-		type: this.type,
+		type: Types[this.type],
 		id: this._id
 	}
 }
