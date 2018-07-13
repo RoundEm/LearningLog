a977d792c68a883cc5804c558894b09a453f93f9
Jason Roundtree <jasontree123@gmail.com>
2018-07-13 12:30:55 -0700

    Update heroku app name

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 function getDbUrl() {
 	let url = '';
 	if (process.env.NODE_ENV === 'production') {
-		url = process.env.DATABASE_URL;
+		url = process.env.DATABASE_URL || 'mongodb://Jason:Go0seMav86@ds249249.mlab.com:49249/learning-log';
 	} else if (process.env.NODE_ENV === 'test') {
 		url = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-learning-log';
 	} 
